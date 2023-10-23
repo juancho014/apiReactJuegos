8d66acef6192e44604d17ee9ae19f0775d77206c
juanfb19@gmail.com <juanfb19@gmail.com>
2023-10-23 20:03:46 -0300

    proyecto2

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const app = express();
 const port =process.env.PORT || 3001;
 const Juego = require('./model/juegos');
+const cors = require('cors');
 
 app.use(express.json());
 
-
+app.use(cors())
 
 
 app.get('/juegos', (req, res) => {
